cbea9032a4b6375759e64ee8233d766d6e53eb1d
lqkke <fonizee@yandex.ru>
2024-10-09 17:56:40 +0300

    Added About section

---
+++ src/layout/sections/About/About.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper";
+import {StyledText} from "../../../components/StyledText";
+import {Link} from "../../../components/Link";
+import {SectionTitle} from "../SectionTitle";
+import {SkillsStats} from "../../../components/skills/SkillsStats";
+
+export const About = () => {
+    return (
+        <StyledAbout>
+            <SectionTitle/>
+            <FlexWrapper justify='space-between'>
+                <FlexWrapper direction='column' justify={'flex-start'}>
+                    <StyledText>Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus
+                        maiores alias consequatur aut perferendis doloribus asperiores repellat quia voluptas sit
+                        aspernatur te natus accusan. maiores alias consequatur aut equatur aut perferendi.</StyledText>
+                    <Link linkType={'about'}/>
+                </FlexWrapper>
+                <SkillsStats/>
+            </FlexWrapper>
+        </StyledAbout>
+    );
+};
+
+const StyledAbout = styled.div``
