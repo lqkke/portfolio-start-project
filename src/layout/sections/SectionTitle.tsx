d487f5f13a957637cdc46880edf406b94a7074cf
lqkke <fonizee@yandex.ru>
2024-10-09 20:21:05 +0300

    SectionTitle refactor started

--- src/layout/sections/SectionTitle.tsx
+++ src/layout/sections/SectionTitle.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import styled from "styled-components";
+import {Icon} from "../../components/icon/Icon";
+import AboutImg from '../../assets/images/about.webp'
 
 export const SectionTitle = () => {
     return (
-        <div>
-            title
-        </div>
+        <StyledSectionTitle>
+            <SectionTitleText>text</SectionTitleText>
+        </StyledSectionTitle>
     );
 };
+
+const StyledSectionTitle = styled.div`
+    background-image: url(${AboutImg});
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 342px;
+    height: 180px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+
+const SectionTitleText = styled.h2`
+`
+
+
 
